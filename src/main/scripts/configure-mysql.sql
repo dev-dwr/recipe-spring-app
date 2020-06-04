f37c5a36b5f122c6093a927f7b7124f19b97184b
dwr <dawid.jscript@gmail.com>
2020-06-04 20:00:32 +0200

    added scripts statements for connecting from any host and localhost

---
+++ src/main/scripts/configure-mysql.sql
@@ -0,0 +1,33 @@
+
+##conntect to mysql and run as root user
+#Create Databases
+CREATE DATABASE app_dev;
+CREATE DATABASE app_prod;
+
+
+#Create database service accounts
+CREATE USER 'app_dev_user'@'localhost' IDENTIFIED BY 'dwr';
+CREATE USER 'app_prod_user'@'localhost' IDENTIFIED BY 'dwr';
+#script statements for connecting from any host
+CREATE USER 'app_dev_user'@'%' IDENTIFIED  BY 'dwr';
+CREATE USER 'app_prod_user'@'%' IDENTIFIED  BY 'dwr';
+
+
+
+#Database grants
+GRANT SELECT ON app_dev.* to 'app_dev_user'@'localhost';
+GRANT INSERT ON app_dev.* to 'app_dev_user'@'localhost';
+GRANT DELETE ON app_dev.* to 'app_dev_user'@'localhost';
+GRANT UPDATE ON app_dev.* to 'app_dev_user'@'localhost';
+GRANT SELECT ON app_prod.* to 'app_prod_user'@'localhost';
+GRANT INSERT ON app_prod.* to 'app_prod_user'@'localhost';
+GRANT UPDATE ON app_prod.* to 'app_prod_user'@'localhost';
+
+GRANT SELECT ON app_dev.* to 'app_dev_user'@'%';
+GRANT INSERT ON app_dev.* to 'app_dev_user'@'%';
+GRANT DELETE ON app_dev.* to 'app_dev_user'@'%';
+GRANT UPDATE ON app_dev.* to 'app_dev_user'@'%';
+GRANT SELECT ON app_prod.* to 'app_prod_user'@'%';
+GRANT INSERT ON app_prod.* to 'app_prod_user'@'%';
+GRANT DELETE ON app_prod.* to 'app_prod_user'@'%';
+GRANT UPDATE ON app_prod.* to 'app_prod_user'@'%';
